2896f3d3a4ca664cba4991af67c71c6b6b006caf
Emanuele Tocci <emato03.felix@gmail.com>
2025-09-07 15:50:54 +0200

    init

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h1>Articoli non trovati 🕵️</h1>
+      <p>La categoria che hai cercato non esiste, prova con un'altra!</p>
+    </div>
+  );
+}
